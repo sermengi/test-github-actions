4ec1ef47a623630491ca7f464b2c0746a71a32b6
sermengi <mengiserkan@outlook.com>
2025-05-07 12:00:36 -0400

    created data validation logic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.datascience import logger
 from src.datascience.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
+from src.datascience.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 
@@ -12,3 +13,15 @@ if __name__ == "__main__":
     except Exception as e:
         logger.exception(e)
         raise e
+
+
+STAGE_NAME = "Data Validation Stage"
+
+try:
+    logger.info(f">>>>>>> stage: {STAGE_NAME} started <<<<<<<<")
+    obj = DataValidationTrainingPipeline()
+    obj.initiate_data_validation()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<< \n\nx========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
--- src/datascience/pipeline/data_ingestion_pipeline.py
+++ src/datascience/pipeline/data_ingestion_pipeline.py
@@ -2,6 +2,7 @@ from src.datascience.config.configuration import ConfigurationManager
 from src.datascience.components.data_ingestion import DataIngestion
 from src.datascience import logger
 
+STAGE_NAME="Data Ingestion Stage"
 
 class DataIngestionTrainingPipeline():
     def __init__(self):
@@ -13,3 +14,14 @@ class DataIngestionTrainingPipeline():
         data_ingestion = DataIngestion(config=data_ingestion_config)
         data_ingestion.download_file()
         data_ingestion.extract_zip_file()
+
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = DataIngestionTrainingPipeline()
+        obj.initiate_data_ingestion()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
