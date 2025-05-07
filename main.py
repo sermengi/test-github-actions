3ad7b79ff3d0de4dabaaaa35169e9d6fe59e1e6c
sermengi <mengiserkan@outlook.com>
2025-05-07 16:36:28 -0400

    created model training logic

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from src.datascience import logger
 from src.datascience.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
 from src.datascience.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
 from src.datascience.pipeline.data_transformation_pipeline import DataTransformationTrainingPipeline
-
+from src.datascience.pipeline.model_training_pipeline import ModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 
@@ -35,6 +35,18 @@ try:
     logger.info(f">>>>>>> stage: {STAGE_NAME} started <<<<<<<<")
     obj = DataTransformationTrainingPipeline()
     obj.initiate_data_transformation()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<< \n\nx========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Model Training Stage"
+
+try:
+    logger.info(f">>>>>>> stage: {STAGE_NAME} started <<<<<<<<")
+    obj = ModelTrainingPipeline()
+    obj.initiate_model_training()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<< \n\nx========x")
 except Exception as e:
     logger.exception(e)
